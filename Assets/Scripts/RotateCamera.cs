de8c1ed1aa38b9b4a1be968e9b8076fd5a20bf56
Revzik <b.piekarz1997@gmail.com>
2019-05-28 16:32:45 +0200

    Add throw strength and random accuracy

--- Assets/Scripts/RotateCamera.cs
+++ Assets/Scripts/RotateCamera.cs
@@ -6,13 +6,14 @@ public class RotateCamera : MonoBehaviour
 {
     public float speedH = 2.0f;
     public float speedV = 2.0f;
-    public float yaw = 0;
-    public float pitch = 0;
+    private float yaw = 0;
+    private float pitch = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+        pitch = transform.eulerAngles.x;
+        yaw = transform.eulerAngles.y;
     }
 
     // Update is called once per frame
--- Assets/Scripts/ThrowToken.cs
+++ Assets/Scripts/ThrowToken.cs
@@ -1,21 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThrowToken : MonoBehaviour
 {
-    public Transform camera;
+    public Transform player;
     public Rigidbody rb;
+    public Slider throwStrength;
     public float distance = 1.0f;
-    public float force = 100.0f;
     public float smoothing = 0.75f;
+    public float accuracy = 5;
     private bool thrown = false;
+    private bool charging = false;
+    private float chargeStart = 0.0f;
+    private float force;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = camera.transform.forward * distance;
-        transform.eulerAngles = camera.transform.eulerAngles;
+        transform.position = player.transform.forward * distance;
+        transform.eulerAngles = player.transform.eulerAngles + new Vector3(0, 0, 90);
+        rb.isKinematic = true;
     }
 
     // Update is called once per frame
@@ -23,13 +29,32 @@ public class ThrowToken : MonoBehaviour
     {
         if (!thrown)
         {
-            transform.position = camera.transform.position + camera.transform.forward * distance;
-            transform.eulerAngles = camera.transform.eulerAngles;
-            if (Input.GetMouseButtonDown(0))
+            transform.position = player.transform.position + player.transform.forward * distance;
+            transform.eulerAngles = player.transform.eulerAngles + new Vector3(0, 0, 90);
+
+            if (!charging && Input.GetMouseButtonDown(0))
             {
+                charging = true;
+                chargeStart = chargeStart = Time.time;
+            }
+
+            if (charging)
+            {
+                force = (Time.time - chargeStart) * 333;
+                if (force > 1000)
+                {
+                    force = 1000;
+                }
+                throwStrength.value = force;
+            }
+
+            if (charging && Input.GetMouseButtonUp(0))
+            {
+                charging = false;
                 thrown = true;
+
                 rb.isKinematic = false;
-                rb.AddForce(camera.transform.forward * force);
+                rb.AddForce(player.transform.forward * force + new Vector3(Random.value * accuracy - accuracy / 2, Random.value * accuracy - accuracy / 2, 0));
             }
         }
     }
